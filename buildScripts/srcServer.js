74f16dd13847248740f5c1f44dcb9b887331e208
roapham <chautrieu@gmail.com>
2017-02-17 15:37:47 +0700

    work with mocking api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -26,6 +26,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    {"id": 1, "firstName": "roa1", "lastName": "pham", "email": "roa1@roa.com"},
+    {"id": 2, "firstName": "roa2", "lastName": 'pham', "email": "roa2@roa.com"},
+    {"id": 3, "firstName": "roa3", "lastName": "pham", "email": "roa3@roa.com"}
+  ]);
+});
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
